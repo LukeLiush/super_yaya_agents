4b6427f9b79d4e8a1104a87f3b90e6bb84e72069
Liu Shuai <liushuai@macbookair.lan>
2025-12-26 17:12:47 -0800

    test secrte tokens

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 
 def main():
     my_secret_token = os.getenv('MY_SECRET_TOKEN')
+    if my_secret_token == "HelloWorld":
+        print("i read correct value")
+    else:
+        print("i read incorrect value: lol")
     print("Hello from super-yaya-agents! here is the test MY_SECRET_TOKEN", my_secret_token)
 
 
